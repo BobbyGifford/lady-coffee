f268b33363b5d751ebc7f013028f3306fe2f5667
Bobby <bobby077joe@gmail.com>
2019-02-15 11:47:06 -0800

    first

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Lady Coffee`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
   },
@@ -24,8 +24,13 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/logo.svg`, // This path is relative to the root of the site.
       },
+
+      /* https://www.iconfinder.com/icons/185113/coffee_streamline_icon
+         Creative Commons (Attribution 3.0 Unported);
+         https://www.iconfinder.com/webalys 
+      */
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
